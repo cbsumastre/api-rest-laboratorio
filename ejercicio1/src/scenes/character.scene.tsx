5b8d332c9315d443685b8600ba04637fbda88671
Carlos Blázquez <admin@firstflip.es>
2025-06-07 17:23:38 +0200

    ejercicio1

---
+++ ejercicio1/src/scenes/character.scene.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { AppLayout } from '#layouts';
+import { CharacterContainer } from '#pods/character-collection';
+
+export const CharacterScene = () => (
+  <AppLayout>
+    <CharacterContainer />
+  </AppLayout>
+);
