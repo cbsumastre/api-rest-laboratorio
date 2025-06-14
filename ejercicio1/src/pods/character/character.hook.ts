49e80b3b173651465f5728705376965e52158ac0
Carlos Blázquez Sumastre <cbsumastre@gmail.com>
2025-06-14 13:42:24 +0200

    rename character.hook

--- ejercicio1/src/pods/character/character.hook.ts
+++ ejercicio1/src/pods/character/character.hook.ts
@@ -1,7 +1,7 @@
 import { getCharacter } from './api';
 import { mapCharacterFromApiToVm } from './character.mapper';
 
-export const useCharacterCollection = (id: string) => {
+export const useCharacter = (id: string) => {
     const loadCharacter = async () => {
         const result = await getCharacter(id);
         return mapCharacterFromApiToVm(result)
